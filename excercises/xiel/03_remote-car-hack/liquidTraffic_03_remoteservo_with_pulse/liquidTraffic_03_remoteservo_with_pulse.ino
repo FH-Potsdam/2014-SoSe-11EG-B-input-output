d124a7c2282a8794f0a86fc5352184023d6bc957
Felix Leupold <mail@felixleupold.de>
2014-07-02 02:16:04 +0200

    liquidTraffic | added bounds and pulse reaction

--- excercises/xiel/03_remote-car-hack/liquidTraffic_03_remoteservo_with_pulse/liquidTraffic_03_remoteservo_with_pulse.ino
+++ excercises/xiel/03_remote-car-hack/liquidTraffic_03_remoteservo_with_pulse/liquidTraffic_03_remoteservo_with_pulse.ino
@@ -7,39 +7,52 @@
  */
  
 /* 
- * just a test to get forward / backward signals from the toy car board
- * added servo
  * now moves accordingly to remote control
  */
  
 //servo
 Servo myservo;
+
+int servoMin = 45;
+int servoMax = 105;
+
 int servoPin = 9;
-int servoCurrentPos = 90;
+int servoCurrentPos = servoMin;
+int servoDirBefore = 0;
 int servoDir = 0;
-int servoStep = 5;
+long updateEvery = 1000 * 10;
+long latestDirUpdate = updateEvery * -1;
+int servoStep = 1;
+int servoLockPosEvery = 10;
+int lockPosToReach = servoCurrentPos;
 
 //forward
-int analogFwdPin = 0;
+int analogFwdPin = A0;
 int valFwd = 0;
 
 //backwards
-int analogBwdPin = 1;
+int analogBwdPin = A1;
 int valBwd = 0;
 
 //pulse (left)
-int pulsePin = 2;
+int pulsePin = A5;
 int valPulse = 0;
+int currentPulseState = 0;
+int pulseAmplitude = 10;
+int pulseStep = 2;
+boolean doPulse = false;
+int pulsePhase = 0;
 
 //misc
 int ledPin = 13;
-
+int maxWaitPoints = 0;
 
 void setup(){
   
   //are we living?
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin,HIGH); 
+  
+  digitalWrite(ledPin, HIGH); 
   
   //servo is connected to digital pin
   myservo.attach(servoPin);
@@ -63,47 +76,132 @@ void loop(){
   int servoBeforePos = servoCurrentPos;
   
   //reset servo moving direction
+  servoDirBefore = servoDir;
   servoDir = 0;
   
   //read analog pins for signals
   valFwd = analogRead(analogFwdPin);
   valBwd = analogRead(analogBwdPin);
-  valPulse = analogRead(pulsePin);
+//  valPulse = analogRead(pulsePin);
+  valPulse = digitalRead(pulsePin);
   
   //log values, if we get a fwd or bwd signal
-  if(valFwd > 100 || valBwd > 100 || valPulse > 100){
+  if(valFwd > 100 || valBwd > 100){
     
     //log the values
-    Serial.print("fwd: ");
-    Serial.println(valFwd);
-    Serial.print("bwd: ");
-    Serial.println(valBwd);
-    Serial.print("pulse (left): ");
-    Serial.println(valPulse);
-    Serial.println("---------");
+//    Serial.print("fwd: ");
+//    Serial.println(valFwd);
+//    Serial.print("bwd: ");
+//    Serial.println(valBwd);
+//    Serial.print("pulse (left): ");
+//    Serial.println(valPulse);
+
+      
+      Serial.println("time since update");
+      Serial.println( (millis() - latestDirUpdate) );
     
     //are we moving forward or backwards?
-    if(valFwd > 50 && valFwd > valBwd && valBwd == 0){
-      servoDir = servoStep;
-    } else if(valBwd > 50 && valFwd < valBwd && valFwd == 0){
-      servoDir = servoStep * -1;
+    if(valFwd > 50 && valFwd > valBwd && valBwd < 50){
+      
+      if( (millis() - latestDirUpdate) > updateEvery ){
+        latestDirUpdate = millis();
+        lockPosToReach = constrain(servoCurrentPos + servoLockPosEvery, servoMin, servoMax);
+      }
+      
+      Serial.println("--- > > > > > fwd");
+    } else if(valBwd > 50 && valFwd < valBwd && valFwd < 50){
+      
+      if( (millis() - latestDirUpdate) > updateEvery ){
+        latestDirUpdate = millis();
+        lockPosToReach = constrain(servoCurrentPos + servoLockPosEvery * -1, servoMin, servoMax);
+      }
+      
+      Serial.println("< < < < < --- bwd");
+    }
+    
+    
+  }
+  
+  Serial.println("lockPosToReach ");
+  Serial.println(lockPosToReach);
+  
+  //check if lock pos is reached
+  if(servoCurrentPos < lockPosToReach){
+    servoDir = servoStep;
+  } else if(servoCurrentPos > lockPosToReach){
+    servoDir = servoStep * -1;
+  }
+  
+  if(valPulse == LOW){
+    Serial.println("PULSE!!! !!! !!! !!!");
+    doPulse = true;
+  } else {
+    doPulse = false;
+  }
+  
+  if(doPulse || pulsePhase != 0){
+    
+    servoDir = 0;
+    
+    if(pulsePhase == 0){
+      pulsePhase = 1;
+    }
+    
+    if(pulsePhase == 1){
+      
+      currentPulseState += pulseStep;
+      servoCurrentPos += pulseStep;
+      
+      if(currentPulseState >= pulseAmplitude){
+         pulsePhase++;
+      }
+      
+      Serial.println("pulsePhase 0");
+    }
+    else if(pulsePhase == 2){
+      
+      currentPulseState += pulseStep * -1;
+      servoCurrentPos += pulseStep * -1;
+      
+      if(currentPulseState <= (pulseAmplitude * -1) ){
+         pulsePhase++;
+      }
+      
+      Serial.println("pulsePhase 1");
+    } else if(pulsePhase == 3) {
+      
+      currentPulseState += pulseStep;
+      servoCurrentPos += pulseStep;
+      
+      if(currentPulseState >= 0){
+        pulsePhase = 0;
+        Serial.println("pulseEnd!");
+      }
+      
+      Serial.println("pulsePhase 3");
     }
   }
   
-  //add direction, if it's not getting out of degree bounds (min 0; max 179)
-  if(servoCurrentPos+servoDir <= 179 && servoCurrentPos+servoDir >= 0){
-    servoCurrentPos = servoCurrentPos + servoDir;
-  }
+  //add direction, if it's not getting out of degree bounds (min 0; max 179)  
+  servoCurrentPos = constrain(servoCurrentPos + servoDir, servoMin, servoMax);
   
   //update servo, if position changed
   if(servoCurrentPos != servoBeforePos){
-    Serial.print("new pos ");
     Serial.println(servoCurrentPos);
     myservo.write(servoCurrentPos);
-    Serial.println("---------");
-    Serial.println("---------");
   }
   
+  //just log something
+  maxWaitPoints++;
+  
+  if(maxWaitPoints < 50){
+    Serial.print(".");
+  } else {
+    Serial.println(".");
+    maxWaitPoints = 0;
+  }
+  
+  
   //give servo time to reach position
-  delay(15);
+  delay( 50 );
 }
